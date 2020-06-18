469d1579555708edf45231e69b3c89ab933f66dc
lawrend <douglas_lawrence@mac.com>
2020-06-18 11:48:35 -0700

    added media breakpoints

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import './css/base.css';
 import BaseContainer from './containers/BaseContainer.js';
 
 function App() {
--- src/containers/BaseContainer.js
+++ src/containers/BaseContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import RightBottom from './RightBottom.js';
 import LeftTop from './LeftTop.js';
+import '../css/base.css';
 
 class BaseContainer extends Component {
   render() {
     return (
-      <div className='base-container'>
+      <div className='flex-div base-container'>
         <LeftTop />
         <RightBottom />
       </div>
