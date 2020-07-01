2361fa0d84f9f34382cf57b86961d97585d2d79d
lawrend <douglas_lawrence@mac.com>
2020-07-01 00:00:41 -0700

    changed styling of bday input and added text

--- src/components/bday/BdayInput.js
+++ src/components/bday/BdayInput.js
@@ -6,8 +6,9 @@ export default props => (
 
   <div className="bday-input-form-container">
   <form className='bday-input-form' onSubmit={props.onSubmit}>
+    <div className='bday-input-label'>IF YOUR LIFE LIVES TO 80, WHICH YOU MAY BE LUCKY TO DO, YOU COULD LOOK BACK AND IMAGINE THAT EACH DAY OF YOUR LIFE WAS A SECOND--YOUR LIFE WOULD BE 8HRS. WHT TM S T NW?</div>
 
-    <label className='bday-input-label'>BIRTHDAY</label>
+    <label className='bday-input-label'>ENTER YOUR BIRTHDAY</label>
 
     <Link to={`results/${props.bday}` }>
       <button type="submit" id="results-button">
@@ -21,5 +22,6 @@ export default props => (
 )
 
 // button above is hidden using the css
+// it is a work around for using Link on submit form
 
 
