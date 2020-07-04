8cafa5af71061fda462e82cecb9431dd55eb714c
lawrend <douglas_lawrence@mac.com>
2020-07-03 17:08:36 -0700

    fixed another rel value a link

--- src/components/homepage/BioBox.js
+++ src/components/homepage/BioBox.js
@@ -4,7 +4,7 @@ class BioBox extends Component {
   render() {
     return (
       <div className='bio-text'>
-        <a href="https://www.linkedin.com/in/douglas-lawrence" target="_blank">
+        <a href="https://www.linkedin.com/in/douglas-lawrence" target="_blank" rel="noopener noreferrer">
         <h3 className='bio-text-title'>
           douglas-lawrence
         </h3>
